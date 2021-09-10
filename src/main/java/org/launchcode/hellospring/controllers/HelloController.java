754fddce601252a22c0772b1ded075f31d7756c2
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-10 13:19:31 -0500

    Complete 10.2.3 @RequestParam

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,20 +1,19 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller // tells Spring Boot that this class represents controllers
 public class HelloController {
 
+    /**
     // handles request at path /hello
     @GetMapping("hello") // tells Spring Boot that this method will only handle GET requests
     @ResponseBody   // tells Spring Boot it will return a plain response
     public String hello() {
         return "Hello, Spring!";
     }
+    */
 
     @GetMapping("goodbye")  // handles request at path /goodbye
     @ResponseBody
@@ -28,4 +27,13 @@ public class HelloController {
     public String helloGoodbye() {
         return "Hello and good bye, Spring!";
     }
+
+    // https://www.youtube.com/watch?v=cXwlynCtZSM
+    // Handles request of the form /hello?name=May
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
 }
